b251b2fcd06b10d76843b901d736326e4760405a
Kneepy <kneepydev@gmail.com>
2024-06-11 11:59:35 +0800

    Create sample queries for datasets and databases tables

---
+++ internal/database/backend/queries/queries.sql
@@ -0,0 +1,18 @@
+-- name: CreateDataset :one
+INSERT INTO datasets (user_id, dataseset_status_id, database_id, table_name)
+VALUES ($1, $2, $3, $4) RETURNING *;
+
+-- name: GetDatasetByID :one
+SELECT * FROM datasets
+WHERE dataset_id = $1 LIMIT 1;
+
+-- name: UpdateDataset :one
+UPDATE datasets
+SET 
+  table_name = $2,
+  is_archieved = $3,
+  archieved_at = $4,
+  dataseset_status_id = $5,
+  database_id = $6
+WHERE dataset_id = $1 RETURNING *;
+
