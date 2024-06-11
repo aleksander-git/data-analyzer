b7823518ef7a5f84f3cf22f582961752ecbd6617
Antonov Denis <den_ru1982@mail.ru>
2024-06-11 13:22:41 +0300

    Add server gRPC and gRPC-gateway to REST

--- test_file.go
+++ test_file.go
@@ -1,7 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log/slog"
+	"net"
+	"net/http"
+
+	pb "github.com/aleksander-git/data-analyzer/grpc/gen/version/v1"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+type MicroserviceServer struct {
+	pb.UnimplementedMicroserviceServer
+}
+
+func (s *MicroserviceServer) GetVersion(ctx context.Context, in *emptypb.Empty) (*pb.VersionResponse, error) {
+	return &pb.VersionResponse{Version: "0.0.1"}, nil
+}
 
 func main() {
-	fmt.Println("Do not delete this file before adding any new main .go files!!!")
+	ch := make(chan error, 2)
+	go func(ch chan error) {
+		slog.Info("gRPC сервер запущен на порту 50051")
+		port, err := net.Listen("tcp", ":50051")
+		if err != nil {
+			ch <- err
+			return
+		}
+		s := grpc.NewServer()
+		reflection.Register(s)
+		pb.RegisterMicroserviceServer(s, &MicroserviceServer{})
+		if err = s.Serve(port); err != nil {
+			ch <- err
+		}
+	}(ch)
+
+	go func(ch chan error) {
+		mux := runtime.NewServeMux()
+		err := pb.RegisterMicroserviceHandlerServer(context.Background(), mux, &MicroserviceServer{})
+		if err != nil {
+			ch <- err
+			return
+		}
+		s := http.Server{
+			Addr:    ":8080",
+			Handler: mux,
+		}
+		slog.Info("gRPC-Gateway сервер запущен на порту 8080")
+		if err = s.ListenAndServe(); err != nil {
+			ch <- err
+			return
+		}
+	}(ch)
+
+	for err := range ch {
+		panic(err)
+	}
 }
