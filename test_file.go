a5f22b124232ae7b85aaf6197b23f8bcce3d5b67
Antonov Denis <den_ru1982@mail.ru>
2024-06-11 15:59:01 +0300

    Add comments

--- test_file.go
+++ test_file.go
@@ -13,10 +13,16 @@ import (
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
+// Создаем свою структуру, которая должна реализовывать интерфейс pb.MicroserviceServer.
+// Здесь взят для примера pb.UnimplementedMicroserviceServer, в котором уже все методы реализованы
+// Но эти методы реализованы для заглушки. И если мы берем эти методы, то нам нужно реализовать только те
+// которые мы хотим посмотреть, а остальные не нужно
+// Сделано чисто для примера
 type MicroserviceServer struct {
 	pb.UnimplementedMicroserviceServer
 }
 
+// GetVersion - возвращает версию нашей программы
 // *emptypb.Empty - пустая структура заглушка для rpc proto файлов
 func (s *MicroserviceServer) GetVersion(ctx context.Context, _ *emptypb.Empty) (*pb.VersionResponse, error) {
 	select {
@@ -33,7 +39,12 @@ const (
 )
 
 func main() {
+	// Создаем буферизированный канал. Если в него будет записана ошибка
+	// то приложение завершится
 	ch := make(chan error, maxServers)
+
+	// Запускаем gRPC сервер локально в горутине,
+	// что бы запустить еще gRPC-Gateway сервер
 	go func(ch chan error) {
 		slog.Info("gRPC сервер запущен на порту 50051")
 		port, err := net.Listen("tcp", "localhost:50051")
@@ -42,29 +53,40 @@ func main() {
 			return
 		}
 		s := grpc.NewServer()
+		// Получаем все методы gRPC сервера. Нужно для того, чтобы программы для работы с API
+		// на подобии Postman, без дополнительного импорта proto файлов знали какие методы доступны.
 		reflection.Register(s)
+
+		// Регистрируем наш интерфейс pb.MicroserviceServer в gRPC сервере
 		pb.RegisterMicroserviceServer(s, &MicroserviceServer{})
+
+		// Стартуем. Если ошибка - пишем в канал ошибок
 		if err = s.Serve(port); err != nil {
 			ch <- err
 		}
 	}(ch)
 
+	// Запускаем наш REST сервер
 	go func(ch chan error) {
+		// Регистрируем наш интерфейс pb.MicroserviceHandler в gRPC-Gateway сервере
 		mux := runtime.NewServeMux()
+		// Регистрируем наш сервер для его дальнейшего использования
 		err := pb.RegisterMicroserviceHandlerServer(context.Background(), mux, &MicroserviceServer{})
 		if err != nil {
 			ch <- err
 			return
 		}
+
+		slog.Info("gRPC-Gateway сервер запущен на порту 8080")
+		// Создаем сервер с нашим обработчиком
 		s := http.Server{
 			Addr:              ":8080",
 			Handler:           mux,
-			ReadHeaderTimeout: 0,
+			ReadHeaderTimeout: 0, // 0 для того, что бы линтер не ругался
 		}
-		slog.Info("gRPC-Gateway сервер запущен на порту 8080")
+		// Стартуем. Если ошибка - пишем в канал ошибок
 		if err = s.ListenAndServe(); err != nil {
 			ch <- err
-			return
 		}
 	}(ch)
 
