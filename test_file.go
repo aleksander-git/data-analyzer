2c86ea1f17edc5bc8a8d6745ca87552bcb1717c8
RR7B7 <fedotovkirill4000@gmail.com>
2024-06-19 00:58:28 +0300

    testing linter

--- test_file.go
+++ test_file.go
@@ -1,7 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	grpc "github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+)
 
 func main() {
 	fmt.Println("Do not delete this file before adding any new main .go files!!!")
+	test, _ := grpc.Bool("true")
+	fmt.Println(test)
 }
